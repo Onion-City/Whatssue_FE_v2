92f73b48c9df44e96fc8a292d55a69a9c26937af
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-04-09 00:13:15 +0900

    Feat: Textarea, Steps, Input, Chip, Button Atoms 추가

--- src/components/atoms/chip/Chip.tsx
+++ src/components/atoms/chip/Chip.tsx
@@ -1,36 +1,24 @@
-import React, { ReactNode, useState } from "react";
+import React, { ReactNode } from "react";
 
 export interface ChipProps {
   children?: ReactNode;
   chipOn?: boolean;
-  width?: string;
-  height?: string;
-  padding?: string;
-  content?: string;
-  borderRadius?: string;
-  fontSize?: string;
-  fontWeight?: string;
+  onClick?: any;
 }
 
-export function Chip({ children, width = "5.4375rem", height, padding = "0.65rem 0", borderRadius = "50px", fontSize = "1rem", fontWeight = "500" }: ChipProps) {
-  const [chipOn, setChipOn] = useState(false);
+export function Chip({ children, chipOn = false, onClick }: ChipProps) {
   const chipStyle: React.CSSProperties = {
     backgroundColor: chipOn ? "#51F8C4" : "#404040",
-    width: width,
-    height: height,
-    padding: padding,
+    width: "5.4375rem",
+    padding: "0.65rem 0",
     color: chipOn ? "#2B2B2B" : "#D9D9D9",
-    borderRadius: borderRadius,
-    fontSize: fontSize,
-    fontWeight: fontWeight,
+    borderRadius: "50px",
+    fontSize: "0.8125rem",
+    fontWeight: "500",
     cursor: "pointer",
     textAlign: "center",
     fontFamily: "Pretendard Variable",
   };
 
-  const handleChip = () => {
-    setChipOn((prev) => !prev);
-  }
-
-  return <span style={chipStyle} onClick={handleChip}>{children}</span>;
+  return <span style={chipStyle} onClick={onClick}>{children}</span>;
 }
