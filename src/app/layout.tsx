0cbd4ee4f1020c2d6afec2cbdb17424f39cac66c
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-04-14 22:57:01 +0900

    Feat: 특정 url 헤더 조건 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,23 @@
+"use client";
 import { HistoryHeader } from "@/components/organisms/Header";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { usePathname } from "next/navigation";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Whatssue",
-  description: "Whatssue",
-};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+
+  const historyHeaderList = [
+    "/user/onboarding",
+    "/user/club"
+  ]
   return (
     <html lang="kr">
-      <body className={inter.className}>
-        <HistoryHeader />
+      <body>
+        {historyHeaderList.includes(pathname) && <HistoryHeader />}
         {children}
       </body>
     </html>
