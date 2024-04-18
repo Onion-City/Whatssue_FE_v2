14a9e61282705e1f2cac5d0a65e97811e322faf7
KKangHHee <skh8609@gmail.com>
2024-04-18 23:05:45 +0900

    Docs: test용 배열 분리

---
+++ src/features/user/board/constants/testArr/TestArr.tsx
@@ -0,0 +1,62 @@
+import testimg from "@/assets/images/chiikyaw.png";
+export const testArr = [
+    {
+      id: 1,
+      title: "30일까지 회비 입금 부탁드립니다",
+      content: "30일까지 회비 입금 부탁드립니다.",
+      date: "1일 전",
+      comment: 5,
+      hearts: 23,
+      contentImg: testimg,
+  
+      isHeart: true,
+      writer: {
+        profile: testimg,
+        name: "신강희",
+      },
+    },
+    {
+      id: 2,
+      title: "30일까지 회비 입금 부탁드립니다",
+      content: "30일까지 회비 입금 부탁드립니다.",
+      date: "1일 전",
+      comment: 5,
+      hearts: 23,
+      contentImg: testimg,
+  
+      isHeart: false,
+      writer: {
+        profile: testimg,
+        name: "신강희",
+      },
+    },
+    {
+      id: 3,
+      title: "30일까지 회비 입금 부탁드립니다",
+      content: "30일까지 회비 입금 부탁드립니다.",
+      date: "1일 전",
+      comment: 5,
+      hearts: 23,
+      contentImg: null,
+  
+      isHeart: true,
+      writer: {
+        profile: testimg,
+        name: "신강희",
+      },
+    },
+  ];
+  export const testArr2 = [
+    {
+      id: 1,
+      title: "떵개 식사 모임",
+      date: "2024.03.05",
+      approval: true,
+    },
+    {
+      id: 2,
+      title: "떵개 식사 모임",
+      date: "2024.03.05",
+      approval: false,
+    },
+  ];
---
+++ src/features/user/home/constants/testArr/TestArr.tsx
@@ -0,0 +1,47 @@
+import testimg from "@/assets/images/chiikyaw.png";
+export const testArr = [
+  {
+    id: 1,
+    title: "파주배드민턴소모임안녕하세요^^123456677",
+    date: "2024.03.05",
+    member: 43,
+    contentImg: testimg,
+    tag: "일반",
+  },
+  {
+    id: 2,
+    title: "싸펑피펑",
+    date: "2024.03.05",
+    member: 313,
+    contentImg: testimg,
+    tag: "일반",
+  },
+  {
+    id: 3,
+    title: "안녕하세요",
+    date: "2024.03.05",
+    member: 43,
+    contentImg: null,
+    tag: "우수",
+  },
+];
+export const testArr2 = [
+  {
+    id: 1,
+    title: "떵개 식사 모임dddddddddddddddddddddddd",
+    date: "2024.03.05",
+    approval: "ACCEPTED",
+  },
+  {
+    id: 2,
+    title: "떵개 기다림 모임",
+    date: "2024.03.05",
+    approval: "WAITING",
+  },
+  {
+    id: 3,
+    title: "떵개 까인 모임",
+    date: "2024.03.05",
+    approval: "REJECTED",
+  },
+];
