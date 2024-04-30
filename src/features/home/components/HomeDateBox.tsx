04e8daa244dcff6f82d63b46962e8a8367cb3e8f
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-05-01 01:57:11 +0900

    Disign: 홈화면 디자인 수정

--- src/features/home/components/HomeDateBox.tsx
+++ src/features/home/components/HomeDateBox.tsx
@@ -12,13 +12,13 @@ const HomeDateBox = ({time, title, onClick}: HomeDateBoxProps) => {
     return (
         <div className="homeDateBox" onClick={onClick}>
             <div className="homeDateBox__content">
-                <span className="wd-20">
+                <span className="wd-30">
                     <Text
                         color="#d9d9d9"
                         fontSize="0.8125rem"
                     >{formatTime(time)}</Text>
                 </span>
-                <span className="wd-70">
+                <span className="wd-50">
                     <Text
                         color="#fff"
                         fontSize="0.9375rem"
@@ -26,7 +26,7 @@ const HomeDateBox = ({time, title, onClick}: HomeDateBoxProps) => {
                     >{title}</Text>
                 </span>
             </div>
-            <span className="align-rt">
+            <span className="homeDateBox__attend wd-20">
                 <Text
                     color="#d9d9d9"
                     fontSize="0.8125rem"
