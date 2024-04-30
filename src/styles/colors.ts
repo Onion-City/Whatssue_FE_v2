b26322976139baca4fd7cf4b44b015cbfedae8f2
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-05-01 01:54:10 +0900

    Rename: styles 폴더 위치 변경

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,6 +1,7 @@
-export const colors = {
+export const COLORS = {
     main: "#51F8C4",
     background: "#2B2B2B",
     red: "#FF4444",
-    white: "#fff"
+    white: "#fff",
+    whitegrey: "#d9d9d9",
 }
