5aaf7fb5bfa72f41224cac4827e32e8731cb72b9
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-05-01 01:56:32 +0900

    Feat: RouterBtn 추가

---
+++ src/components/organisms/RouterBtn/RouterBtn.tsx
@@ -0,0 +1,24 @@
+import { useRouter } from "next/navigation";
+import React from "react";
+
+interface routerBtnProps {
+    path: string;
+    onClick?: any;
+    children: React.ReactNode;
+};
+
+export const RouterBtn = ({path, onClick, children}: routerBtnProps) => {
+    const router = useRouter();
+    const handleRouter = () => {
+        onClick();
+        router.push(path);
+    }
+    return (
+        <div 
+            onClick={handleRouter} 
+            style={{ cursor: 'pointer' }}
+        >
+            {children}
+        </div>
+    )
+}
