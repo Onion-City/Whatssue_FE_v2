b59da2f153f623153ab6f682cb54a585832ef248
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-05-21 18:27:45 +0900

    Feat: ContextAPI를 이용하여 일정 상태 관리

--- src/features/home/components/HomeHeader.tsx
+++ src/features/home/components/HomeHeader.tsx
@@ -1,10 +1,11 @@
 import { HomeCalendar } from "@/components/atoms/calendar";
 import { Search } from "@/components/atoms/search";
-import { useState } from "react";
+import { useContext } from 'react';
+import { ScheduleContext } from '../SetHome'; // Adjust the path as needed
 import "./Home.css";
 
 const HomeHeader = () => {
-    const [value, onChange] = useState<Date>(new Date());
+    const { value, onChange } = useContext(ScheduleContext);
     return (
         <div className="homeHeader">
             <Search />
