ac7a68cf6e852cf6fbf8dbb764e508a2e83f0051
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-05-21 18:28:23 +0900

    Feat: 카카오 로그인 추가(진행중)

--- src/features/user/Login/components/LoginBtn.tsx
+++ src/features/user/Login/components/LoginBtn.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+"use client";
 import { IMAGES } from "@/constants/images";
+import Image from "next/image";
 import "./Login.css";
 
 export default function LoginBtn() {
+  const handleLogin = () => {
+    window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_API_KEY}&redirect_url=${process.env.NEXT_PUBLIC_REDIRECT_URL}&response_type=code`
+  }
   return (
-    <Image src={IMAGES.loginKakao} alt="kakao login" className="login_kakao" />
+    <Image src={IMAGES.loginKakao} alt="kakao login" className="login_kakao" onClick={handleLogin}/>
   );
 }
