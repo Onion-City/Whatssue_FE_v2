885bab2ed33063d866382ed55dcd2e9c648ea882
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-05-27 21:44:55 +0900

    Feat: withCredentials 옵션 추가

--- src/apis/http.tsx
+++ src/apis/http.tsx
@@ -3,7 +3,8 @@ import axios from "axios";
 import { AxiosInstance, AxiosRequestConfig } from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_LOCAL_URL
+    baseURL: process.env.NEXT_PUBLIC_LOCAL_URL,
+    withCredentials: true
 });
 
 export interface HttpClient extends AxiosInstance {
