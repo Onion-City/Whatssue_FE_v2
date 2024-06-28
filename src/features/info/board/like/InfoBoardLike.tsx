aff526ab01193f63d0f352d9ab17563f223211b9
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-06-28 16:35:54 +0900

    Feat: 좋아요 한 글 custom hook (useMyPostLikeQuery)

---
+++ src/features/info/board/like/InfoBoardLike.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { HistoryHeader } from "@/components/organisms/Header";
+import { Nav } from "@/components/organisms/Nav";
+import { IMAGES } from "@/constants/images";
+import BoardItem from "@/features/board/components/BoardItem";
+import useMyPostLikeQuery from "@/hook/info/useMyPostLikeQuery";
+
+import "./components/BoardLike.css";
+
+export const InfoBoardLike = () => {
+    const { data: boardInfo } = useMyPostLikeQuery({
+        clubId: 7,
+        userId: 6,
+        category: "FREE",
+        page: 0,
+        size: 10,
+    });
+    console.log(boardInfo);
+
+    return(
+        <>
+            <HistoryHeader 
+                title="내가 좋아요 글"
+            />
+            {/* TODO: skeleton UI 적용 */}
+            {
+                !boardInfo || !boardInfo.data ? 
+                <div>Loading...</div> :
+                (
+                    <div className="infoBoard">
+                        {boardInfo?.data?.content?.map((item: any) => (
+                            <BoardItem
+                                key={item.postId}
+                                // boardAddress={boardTypeAddress}
+                                id={item.postId}
+                                title={item.postTitle}
+                                content={item.postContent}
+                                createdAt={item.createdAt}
+                                commentCount={item.commentCount}
+                                contentImgs={item.uploadImage ?? []}
+                                hearts={item.postLikeCount}
+                                isHeart={item.isLiked}
+                                writer={{ name: item.writerName, profile: item.writerProfileImage ?? IMAGES.profile }}
+                            />
+                        ))}
+                    </div>
+                )
+            }
+            <Nav />
+        </>
+    )
+};
