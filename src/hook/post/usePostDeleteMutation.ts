a1d48a6b20f857b97c8469c8b3ad9d3cac8f1ac9
KKangHHee <skh8609@gmail.com>
2024-07-06 19:32:45 +0900

    Feat: react-toastify 일부 적용

--- src/hook/post/usePostDeleteMutation.ts
+++ src/hook/post/usePostDeleteMutation.ts
@@ -1,5 +1,6 @@
 import { http } from "@/apis/http";
 import { FormatClubId, FormatPostId } from "@/utils/extractPathElements";
+import useToast from "@/utils/useToast";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 
@@ -8,13 +9,24 @@ export const usePostDeleteMutation = () => {
   const postId = FormatPostId();
   const queryClient = useQueryClient();
   const router = useRouter();
+  const { showToast } = useToast();
   return useMutation({
     mutationFn: () => http.delete(`/clubs/${clubId}/posts/${postId}`),
     onSuccess: () => {
-      alert("삭제되었습니다!");
+      showToast({
+        message: "게시글 삭제 성공",
+        type: "success",
+      });
       router.back();
       queryClient.invalidateQueries({
         queryKey: ["postList"],
+      });
+    },
+    onError: (error) => {
+      console.log("게시글 삭제 실패", error);
+      showToast({
+        message: "게시글 삭제 실패",
+        type: "error",
       });
     },
   });
--- src/hook/post/usePostMutation.ts
+++ src/hook/post/usePostMutation.ts
@@ -1,5 +1,6 @@
 import { http } from "@/apis/http";
 import { FormatClubId } from "@/utils/extractPathElements";
+import useToast from "@/utils/useToast";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 
@@ -7,6 +8,7 @@ export const usePostMutation = () => {
   const router = useRouter();
   const queryClient = useQueryClient();
   const clubId = FormatClubId();
+  const { showToast } = useToast();
   return useMutation({
     mutationFn: (data: FormData) => http.post(`/clubs/${clubId}/posts`, data),
     onSuccess: () => {
@@ -14,10 +16,18 @@ export const usePostMutation = () => {
         queryKey: ["postList"],
       });
       console.log("게시글 등록 성공");
+      showToast({
+        message: "게시글이 등록되었습니다.",
+        type: "success",
+      });
       router.back();
     },
     onError: (error) => {
       console.log("게시글 등록 실패", error);
+      showToast({
+        message: "게시글 등록 실패",
+        type: "error",
+      });
     },
   });
 };
