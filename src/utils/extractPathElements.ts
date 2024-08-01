eab8bf087f86eae9bb013a3984d6e50cae9e192b
KKangHHee <skh8609@gmail.com>
2024-08-01 20:30:27 +0900

    Docs: CSR일 때 실행으로 변경

--- src/utils/extractPathElements.ts
+++ src/utils/extractPathElements.ts
@@ -1,11 +1,17 @@
-"use client";
 import { RootState } from "@/redux/store";
 import { usePathname, useRouter } from "next/navigation";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 export function FormatClubId() {
   const router = useRouter();
   const clubId = useSelector((state: RootState) => state.club.clubId);
+  useEffect(() => {
+    if (clubId === undefined || clubId === null) {
+      console.log(`클럽아이디 설정안됨`);
+      router.push("/");
+    }
+  }, [clubId, router]);
   if (clubId === undefined || clubId === null) {
     console.log(`클럽아이디 설정안됨`);
     router.push("/");
