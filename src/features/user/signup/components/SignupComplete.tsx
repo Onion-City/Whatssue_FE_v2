ec8174b37d5004ad7d5fcabf70a3ec634e708ca2
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-08-07 21:42:56 +0900

    Feat: AuthBox을 InputBox에서 분리

--- src/features/user/signup/components/SignupComplete.tsx
+++ src/features/user/signup/components/SignupComplete.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/atoms/button";
 import { Text } from "@/components/atoms/text";
 import { Wrapper } from "@/components/organisms/Wrapper";
 import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 import { SIGNUP_COMPLETE_BTN, SIGNUP_COMPLETE_TXT } from "../constants";
 import "./Signup.css";
 
@@ -12,27 +13,29 @@ const SignupComplete = () => {
     const searchParams = useSearchParams();
     const userName = searchParams.get('userName');
     return(
-        <Wrapper>
-            <div className="signup__content">
-                <div className="signup__text">
-                    <Text
-                        color="#fff"
-                        fontSize="1.0625rem"
-                        fontWeight="700"
-                    >{userName}{SIGNUP_COMPLETE_TXT[0]}</Text>
+        <Suspense fallback="Loading...">
+            <Wrapper>
+                <div className="signup__content">
+                    <div className="signup__text">
+                        <Text
+                            color="#fff"
+                            fontSize="1.0625rem"
+                            fontWeight="700"
+                        >{userName}{SIGNUP_COMPLETE_TXT[0]}</Text>
+                    </div>
+                    <div className="signup__text">
+                        <Text
+                            color="#fff"
+                            fontSize="1.0625rem"
+                            fontWeight="700"
+                        >{SIGNUP_COMPLETE_TXT[1]}</Text>
+                    </div>
                 </div>
-                <div className="signup__text">
-                    <Text
-                        color="#fff"
-                        fontSize="1.0625rem"
-                        fontWeight="700"
-                    >{SIGNUP_COMPLETE_TXT[1]}</Text>
+                <div className="register3_rowBtn">
+                    <Button size="lg" onClick={() => router.push("/")}>{SIGNUP_COMPLETE_BTN}</Button>
                 </div>
-            </div>
-            <div className="register3_rowBtn">
-                <Button size="lg" onClick={() => router.push("/")}>{SIGNUP_COMPLETE_BTN}</Button>
-            </div>
-        </Wrapper>
+            </Wrapper>
+        </Suspense>
     )
 };
 
