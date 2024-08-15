d50ea984386350b9d4d1ee92d581ac0eeaae7832
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-08-15 19:06:57 +0900

    Fix: auth 에러 처리

--- src/hook/member/useMemberAuthQuery.ts
+++ src/hook/member/useMemberAuthQuery.ts
@@ -1,18 +1,40 @@
 import { http } from "@/apis/http";
 import { MemberAuthInfo } from "@/types/member";
 import { FormatClubId } from "@/utils/extractPathElements";
+import useToast from "@/utils/useToast";
 import { useQuery } from "@tanstack/react-query";
 import { AxiosError, AxiosResponse } from "axios";
+import { useRouter } from "next/navigation";
 // 클럽 auth 조회
 export const useMemberAuthQuery = () => {
+  const { showToast } = useToast();
+  const router = useRouter();
   const clubId = FormatClubId();
   return useQuery<AxiosResponse<MemberAuthInfo>, AxiosError>({
     queryKey: ["myClubAuth", [clubId]],
     queryFn: async () => {
-      const response = await http.get<AxiosResponse<MemberAuthInfo>>(
-        `/clubs/${clubId}/member/my/auth`
-      );
-      return response;
+      try {
+        const response = await http.get<AxiosResponse<MemberAuthInfo>>(
+          `/clubs/${clubId}/member/my/auth`
+        );
+        return response;
+      } catch (error) {
+        const axiosError = error as AxiosError<any>;
+        if (axiosError.response?.data.code === "2200") {
+          router.push("/user/onboarding?policy=REAL_NAME");
+          // showToast({
+          //   message: `${axiosError.response.data.message}`,
+          //   type: "error",
+          // });
+        } else {
+          showToast({
+            message: "에러가 발생했습니다.",
+            type: "error",
+          });
+          router.push("/");
+        }
+        throw error;
+      }
     },
   });
 };
