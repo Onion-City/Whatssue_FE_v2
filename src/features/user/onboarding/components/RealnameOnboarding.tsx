8e9a978bcf2c697733beb375950cf42f56e166d9
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-08-15 17:27:04 +0900

    Feat: 멤버 프로필 초기 설정 custom hook (useMemberProfileMutation)

--- src/features/user/onboarding/components/RealnameOnboarding.tsx
+++ src/features/user/onboarding/components/RealnameOnboarding.tsx
@@ -1,83 +1,125 @@
+import AddPic from "@/assets/images/AddPic.png";
 import { Button } from "@/components/atoms/button";
-import { FileUpload } from "@/components/atoms/fileUpload";
 import { Text } from "@/components/atoms/text";
 import { InputBox } from "@/components/molecules/inputBox";
 import { Wrapper } from "@/components/organisms/Wrapper";
+import { MEMBERPROFILE_TITLE } from "@/features/member/profileEdit/constants/const";
 import { useMemberProfileMutation } from "@/hook/member/useMemberProfileMutation";
-import { COLORS } from "@/styles";
+import Image from "next/image";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { ONBOARDING_BTN, ONBOARDING_INPUT_ARR } from "../constants/const";
 
 interface FormData {
-    memberName: string;
-    memberIntro: string;
-    profileImage: string | File;
-    isEmailPublic: boolean;
-    isPhonePublic: boolean;
+  memberName: string;
+  memberIntro: string;
+  profileImage: string | File;
+  isEmailPublic: boolean;
+  isPhonePublic: boolean;
 }
 
 const RealnameOnboarding = () => {
-    const { control, handleSubmit, setValue } =
-    useForm<FormData>({
-      mode: "onChange",
-      defaultValues: {
-        memberName: '',
-        memberIntro: '',
-        profileImage: '',
-        isEmailPublic: false,
-        isPhonePublic: false,
-      }
-    });
-    const [imageUrl, setImageUrl] = useState<string | undefined>();
-    const handleChangeImage = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const files = e.target.files;
-        if (!files || files.length === 0) return;
-        const selectedFile = files[0];
-        const newImageUrl = URL.createObjectURL(selectedFile);
-        setImageUrl(newImageUrl);
-        setValue("profileImage", selectedFile);
-    };
+  const { control, handleSubmit, setValue } = useForm<FormData>({
+    mode: "onChange",
+    defaultValues: {
+      memberName: "",
+      memberIntro: "",
+      profileImage: "",
+      isEmailPublic: false,
+      isPhonePublic: false,
+    },
+  });
+  const [imageUrl, setImageUrl] = useState<string | undefined>();
+  const handleChangeImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (!files || files.length === 0) return;
+    const selectedFile = files[0];
+    const newImageUrl = URL.createObjectURL(selectedFile);
+    setImageUrl(newImageUrl);
+    setValue("profileImage", selectedFile);
+  };
 
-    const { mutate } = useMemberProfileMutation()
-    const submitOnboarding = (data: FormData) => {
-        console.log(data);
-        // mutate(data);
-    }
-    return (
-        <form onSubmit={handleSubmit(submitOnboarding)}>
-            <Wrapper isHeader={true}>
-                <div className="register__content">
-                    <div className="register__content__img">
-                        <Text
-                            color={COLORS.white}
-                            fontSize="1.0625rem"
-                            fontWeight="700"
-                        >프로필 사진</Text>
-                        <FileUpload />
-                    </div>
-                    {ONBOARDING_INPUT_ARR.realname.map((box, index) => (
-                        <React.Fragment key={index}>
-                            <InputBox 
-                                title={box.title && box.title}
-                                maxCnt={box.maxCnt && box.maxCnt}
-                                type={box.type && box.type}
-                                subtitle={box.subtitle && box.subtitle}
-                                essential={box.essential && box.essential}
-                                name={box.name && box.name}
-                                control={control}
-                            />
-                        </React.Fragment>
-                    ))}
+  const postFormData = (propData: FormData) => {
+    const resData = new FormData();
+    resData.append("memberName", propData.memberName);
+    resData.append("memberIntro", propData.memberIntro);
+    resData.append("isEmailPublic", String(propData.isEmailPublic));
+    resData.append("isPhonePublic", String(propData.isPhonePublic));
+    resData.append("profileImage", propData.profileImage);
+
+    return resData;
+  };
+
+  const { mutate } = useMemberProfileMutation();
+  const submitOnboarding = (data: FormData) => {
+    console.log(data);
+    const PostData = postFormData(data);
+    console.log(PostData);
+    mutate(PostData);
+  };
+  return (
+    <form onSubmit={handleSubmit(submitOnboarding)}>
+      <Wrapper isHeader={true}>
+        <div className="register__content">
+          <div className="register__content__img">
+            <Text color="#fff" fontSize="1.0625rem" fontWeight="700">
+              {MEMBERPROFILE_TITLE}
+            </Text>
+            <>
+              <label htmlFor="first-upload-input">
+                <div className="fileUpload__box">
+                  {imageUrl ? (
+                    <Image
+                      src={imageUrl}
+                      alt="pic"
+                      className="fileUpload__box__img default"
+                      placeholder="blur"
+                      blurDataURL="@/assets/images/AddPic.png"
+                      width={115}
+                      height={115}
+                    />
+                  ) : (
+                    <Image
+                      src={AddPic}
+                      alt="pic"
+                      className="fileUpload__box__img"
+                      placeholder="blur"
+                      blurDataURL="@/assets/images/AddPic.png"
+                      width={115}
+                      height={115}
+                    />
+                  )}
                 </div>
-                <div className="register__btn">
-                    <Button
-                        type="submit"
-                    >{ONBOARDING_BTN}</Button>
-                </div>
-            </Wrapper>
-        </form>
-    )
-}
+              </label>
+              <input
+                id="first-upload-input"
+                type="file"
+                accept="image/*"
+                name="profileImage"
+                onChange={handleChangeImage}
+              />
+            </>
+          </div>
+          {ONBOARDING_INPUT_ARR.realname.map((box, index) => (
+            <React.Fragment key={index}>
+              <InputBox
+                title={box.title && box.title}
+                maxCnt={box.maxCnt && box.maxCnt}
+                type={box.type && box.type}
+                subtitle={box.subtitle && box.subtitle}
+                essential={box.essential && box.essential}
+                name={box.name && box.name}
+                control={control}
+              />
+            </React.Fragment>
+          ))}
+        </div>
+        <div className="register__btn">
+          <Button type="submit">{ONBOARDING_BTN}</Button>
+        </div>
+      </Wrapper>
+    </form>
+  );
+};
 
 export default RealnameOnboarding;
--- src/hook/member/useMemberProfileMutation.ts
+++ src/hook/member/useMemberProfileMutation.ts
@@ -2,6 +2,7 @@ import { http } from "@/apis/http";
 import { FormatClubId } from "@/utils/extractPathElements";
 import useToast from "@/utils/useToast";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { AxiosError } from "axios";
 import { useRouter } from "next/navigation";
 
 export function useMemberProfileMutation() {
@@ -26,10 +27,23 @@ export function useMemberProfileMutation() {
     },
     onError: (error) => {
       console.log("프로필 등록 실패", error);
-      showToast({
-        message: "프로필 등록 실패",
-        type: "error",
-      });
+      if ((error as AxiosError).isAxiosError) {
+        const axiosError = error as AxiosError<any>;
+        if (axiosError.response) {
+          if (axiosError.response.data.errors) {
+            showToast({
+              message: `${axiosError.response.data.errors[0].message}`,
+              type: "error",
+            });
+          } else {
+            showToast({
+              message: `${axiosError.response.data.message}`,
+              type: "error",
+            });
+            if (axiosError.response.data.code === "2201") router.back();
+          }
+        }
+      }
     },
   });
 }
