5b0712ddcd2bf59d4964c98d4bf28046ad46c37b
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-08-16 20:54:39 +0900

    Fix: 공결 신청 api 오류 수정

--- src/features/home/calendar/components/CalendarDetailBtn.tsx
+++ src/features/home/calendar/components/CalendarDetailBtn.tsx
@@ -1,76 +1,77 @@
 import { Button } from "@/components/atoms/button";
 import { Textarea } from "@/components/atoms/textarea";
 import { Modal } from "@/components/organisms/Modal/Modal";
-import { useModalContext } from "@/components/organisms/Modal/ModalProvider";
 import { useAbsenceReqMutation } from "@/hook/absence/useAbsenceReqMutation";
 import { COLORS } from "@/styles";
+import { FormatClubId } from "@/utils/extractPathElements";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 import { ABSENT_MODAL, CALENDAR_BTN } from "../constants/const";
 import "./Calendar.css";
 
 interface CalendarDetailBtnProps {
-    // scheduleId: number;
-    scheduleName: string | undefined;
+  // scheduleId: number;
+  scheduleName: string | undefined;
 }
 
-const CalendarDetailBtn = ({ 
-    // scheduleId,
-    scheduleName 
+const CalendarDetailBtn = ({
+  // scheduleId,
+  scheduleName,
 }: CalendarDetailBtnProps) => {
-    const path = usePathname();
-    const { isOpen, openModal, closeModal } = useModalContext();
-    // const { register, handleSubmit } = useForm<AbsenceReasonValues>();
-    const [absenceReason, setAbsenceReason] = useState("");
-    const { mutate } = useAbsenceReqMutation({
-        clubId: 1,
-        scheduleId: +path.split("/")[3],
+  const path = usePathname();
+  const clubId = FormatClubId();
+
+  const [isOpen, setIsOpen] = useState(false);
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
+  const [absenceReason, setAbsenceReason] = useState("");
+  const { mutate } = useAbsenceReqMutation({
+    clubId: clubId,
+    scheduleId: +path.split("/")[3],
+  });
+
+  const onSubmit = () => {
+    closeModal();
+    setAbsenceReason("");
+    mutate({
+      officialAbsenceContent: absenceReason,
     });
+  };
 
-    const onSubmit = () => {
-        closeModal();
-        mutate({
-            clubMemberId: 14,
-            scheduleId: +path.split("/")[3],
-            officialAbsenceContent: absenceReason
-        });
-    }
+  return (
+    <div className="calendarDetailBtn">
+      <Button backgroundColor="#fff" onClick={openModal}>
+        {CALENDAR_BTN.absent}
+      </Button>
 
-    return (
-            <div className="calendarDetailBtn">
-                <Button 
-                    backgroundColor="#fff"
-                    onClick={openModal}
-                >{CALENDAR_BTN.absent}</Button>
-                <Button>{CALENDAR_BTN.attendance}</Button>
-
-                {/* Modal */}
-                <Modal isOpen={isOpen}>
-                    <Modal.Header>
-                        <Modal.Title>{scheduleName ?? ""}</Modal.Title>
-                        <Modal.Subtitle>{ABSENT_MODAL.member_subtitle}</Modal.Subtitle>
-                    </Modal.Header>
-                    <Modal.Content>
-                        <Textarea
-                            backgroundColor={COLORS.white}
-                            color={COLORS.background}
-                            size="pr"
-                            height="80%"
-                            isBorder={true}
-                            value={absenceReason}
-                            onChange={(value: string) => setAbsenceReason(value)}
-                        ></Textarea>
-                    </Modal.Content>
-                    <Modal.Footer>
-                        <Modal.Button
-                            onClick={onSubmit}
-                        >
-                            {ABSENT_MODAL.member_btn}
-                        </Modal.Button>
-                    </Modal.Footer>
-                </Modal>
-            </div>
-    )
+      {/* Modal */}
+      <Modal isOpen={isOpen}>
+        <Modal.Header closeModal={closeModal}>
+          <Modal.Title>{scheduleName ?? ""}</Modal.Title>
+          <Modal.Subtitle>{ABSENT_MODAL.member_subtitle}</Modal.Subtitle>
+        </Modal.Header>
+        <Modal.Content>
+          <div className="calendarDetailBtn__textarea">
+            <Textarea
+              backgroundColor={COLORS.white}
+              color={COLORS.background}
+              size="pr"
+              height="80%"
+              isBorder={true}
+              value={absenceReason}
+              onChange={(value: string) => setAbsenceReason(value)}
+            ></Textarea>
+          </div>
+        </Modal.Content>
+        <Modal.Footer>
+          <Modal.Button onClick={onSubmit}>
+            {ABSENT_MODAL.member_btn}
+          </Modal.Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 };
 
 export default CalendarDetailBtn;
--- src/hook/absence/useAbsenceReqMutation.tsx
+++ src/hook/absence/useAbsenceReqMutation.tsx
@@ -1,19 +1,55 @@
-import { http } from "@/apis/http"
-import { AbsenceReqData, AbsenceReqProps } from "@/types/absence/types"
-import { useMutation } from "@tanstack/react-query"
+import { http } from "@/apis/http";
+import { AbsenceReqData, AbsenceReqProps } from "@/types/absence/types";
+import useToast from "@/utils/useToast";
+import { useMutation } from "@tanstack/react-query";
+import { AxiosError, AxiosResponse } from "axios";
 
 // 공결 신청 (/{clubId}/official_absence/request/{scheduleId})
 export const useAbsenceReqMutation = ({
   clubId,
-  scheduleId
+  scheduleId,
 }: AbsenceReqProps) => {
-  return useMutation({
-    mutationFn: (data: AbsenceReqData) => http.post(`/${clubId}/official_absence/request/${scheduleId}`, data),
-    onSuccess: (data) => {
-      console.log(data)
-      // if(data?.status === 200){
+  const { showToast } = useToast();
 
-      // }
-    }
-  })
-}
+  return useMutation<AxiosResponse<string>, Error, AbsenceReqData>({
+    mutationFn: (data: AbsenceReqData) => {
+      const body = data.officialAbsenceContent;
+      return http.post(
+        `/${clubId}/official-absence/request/${scheduleId}`,
+        body
+      );
+    },
+    onSuccess: (data) => {
+      console.log(data);
+      if (data.status === 200) {
+        showToast({
+          message: "공결 신청되었습니다.",
+          type: "success",
+        });
+      } else {
+        showToast({
+          message: data.data,
+          type: "error",
+        });
+      }
+    },
+    onError: (error) => {
+      if ((error as AxiosError).isAxiosError) {
+        const axiosError = error as AxiosError<any>;
+        if (axiosError.response) {
+          if (axiosError.response.data.code === "6200") {
+            showToast({
+              message: "이미 공결 신청한 일정입니다.",
+              type: "error",
+            });
+          } else {
+            showToast({
+              message: `${axiosError.response.data.message}`,
+              type: "error",
+            });
+          }
+        }
+      }
+    },
+  });
+};
