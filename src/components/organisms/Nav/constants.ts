85e9049b5aa00ed8a10eda6d2066f8df133a9836
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-08-20 23:18:25 +0900

    Feat: 권한 별 출석 페이지 분리

--- src/components/organisms/Nav/constants.ts
+++ src/components/organisms/Nav/constants.ts
@@ -8,7 +8,7 @@ interface NavItem {
 export const navItems: NavItem[] = [
   { path: "club", icon: NavIcons.home, text: "홈" },
   { path: "club/board", icon: NavIcons.board, text: "게시판" },
-  { path: "attendance", icon: NavIcons.atten, text: "출석" },
+  { path: "club/attendance", icon: NavIcons.atten, text: "출석" },
   { path: "member/list", icon: NavIcons.member, text: "맴버" },
   { path: "info", icon: NavIcons.more, text: "더보기" },
 ];
