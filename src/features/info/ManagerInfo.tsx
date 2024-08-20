887a92218de27ca9ba43fd9299dabbc8496a014a
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-08-20 23:13:16 +0900

    Design: 더보기 페이지 디자인 수정

--- src/features/info/ManagerInfo.tsx
+++ src/features/info/ManagerInfo.tsx
@@ -3,19 +3,18 @@ import { Wrapper } from "@/components/organisms/Wrapper";
 import { MemberBtn } from "./components/MemberBtn";
 import { MemberContent } from "./components/MemberContent";
 
-export default function ManagerInfo () {
-    return(
-        <>
-            <Wrapper isHeader={true}>
-                <div style={{ paddingTop: '4.25rem' }}>   
-                    <MemberContent />
-                </div>
-                <div className="memberInfo">
-                    <MemberBtn />
-                    <Nav />
-                </div>
-            </Wrapper>
-        </>
-
-    )
+export default function ManagerInfo() {
+  return (
+    <>
+      <Wrapper isHeader={true}>
+        <div style={{ marginTop: "4.25rem" }}>
+          <MemberContent />
+        </div>
+        <div className="memberInfo">
+          <MemberBtn />
+          <Nav />
+        </div>
+      </Wrapper>
+    </>
+  );
 }
