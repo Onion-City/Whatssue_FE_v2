53b1d229a6d31857fcff11a68642fff7822ca389
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-08-25 17:03:41 +0900

    Fix: 공결 신청 수락 & 거절 오류 해결

--- src/features/info/manage/absent/components/ManageAbsentHistoryList.tsx
+++ src/features/info/manage/absent/components/ManageAbsentHistoryList.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useAbsencesDoneQuery } from "@/hook/absence/useAbsencesDoneQuery";
-import { AbsenceMemberData } from "@/types/absence/types";
 import { FormatClubId } from "@/utils/extractPathElements";
 import { useState } from "react";
 
@@ -9,44 +8,24 @@ import "./ManageAbsent.css";
 import { ManageAbsentHistoryItem } from "./ManageAbsentHistoryItem";
 
 export default function ManageAbsentHistoryList() {
-  const absentDummy: AbsenceMemberData = 
-  {
-    id: 0,
-    clubMemberId: 123,
-    scheduleId: 456,
-    scheduleDate: "2024-01-01",
-    scheduleName: "와이어 프레임 작성 회의",
-    officialAbsenceContent: "test공결사유",
-    officialAbsenceRequestType: "WAITING",
-    createAt: "2024-01-01",
-    updateAt: "2024-01-01"
-  }
-  
   const [page, setPage] = useState(0);
   const [params, setParams] = useState({
     clubId: FormatClubId(),
     startDate: "2000-01-01",
     endDate: "2030-01-01",
     page: page,
-    size: 10
-  })
+    size: 10,
+  });
   const { data: doneList } = useAbsencesDoneQuery(params);
   console.log(doneList?.data);
-  
+
   return (
     <div className="historyList">
-      {
-        doneList?.data?.content
-        ?.map(item => 
-          <span 
-            key={item.id}
-          >
-            <ManageAbsentHistoryItem
-              data={item}
-            />
-          </span>
-        )
-      }
+      {doneList?.data?.content?.map((item) => (
+        <span key={item.id}>
+          <ManageAbsentHistoryItem data={item} />
+        </span>
+      ))}
     </div>
-  )
+  );
 }
