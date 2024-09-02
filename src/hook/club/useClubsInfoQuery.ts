5fc5eddcbfffe4080d4be7ccdddc2f4fc209e2fc
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-09-03 03:07:31 +0900

    Fix: 프로필 설정 닉네임 & 실명 페이지 구분

--- src/hook/club/useClubsInfoQuery.ts
+++ src/hook/club/useClubsInfoQuery.ts
@@ -1,18 +1,18 @@
 import { http } from "@/apis/http";
-import { CommonNoPageRes } from "@/types";
 import { ClubInfoContent, ClubInfoParams } from "@/types/club";
 import { useQuery } from "@tanstack/react-query";
+import { AxiosResponse } from "axios";
 
 // 모임 정보 조회 (/clubs/{clubId}/info)
 export const useClubsInfoQuery = ({ clubId }: ClubInfoParams) => {
-    return useQuery<CommonNoPageRes<ClubInfoContent>>({
-        queryKey: ['clubInfo', [clubId]],
-        queryFn: async () => {
-            const response = await http.get<CommonNoPageRes<ClubInfoContent>>(
-                `/clubs/${clubId}/info`
-            );
-            return response;
-        },
-        staleTime: 1000,
-    });
+  return useQuery<Promise<AxiosResponse<ClubInfoContent>>>({
+    queryKey: ["clubInfo", [clubId]],
+    queryFn: async () => {
+      const response = await http.get<Promise<AxiosResponse<ClubInfoContent>>>(
+        `/clubs/${clubId}/info`
+      );
+      return response;
+    },
+    staleTime: 1000,
+  });
 };
